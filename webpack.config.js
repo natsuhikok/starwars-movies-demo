bb35eeaac304848b131fc31e1e91598e5c5e8663
natsuhikok <kato@natsuhiko.jp>
2018-02-27 22:14:17 +1300

    Update: Change webpack entry point to index.js from app.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    bundle: './src/scripts/app.js',
+    bundle: './src/scripts/index.js',
   },
   output: {
     path: path.join(__dirname, 'public/assets/scripts'),
