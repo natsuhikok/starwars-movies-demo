baf21ad12b7c326316129e12b72a2bb056c7edf8
natsuhikok <kato@natsuhiko.jp>
2018-05-07 00:01:34 +1200

    Add: render list of planets and characters

--- src/scripts/components/Episode.js
+++ src/scripts/components/Episode.js
@@ -4,39 +4,43 @@ import { bindActionCreators } from 'redux';
 import { fetchEpisode, fetchEpisodePlanets, fetchEpisodeCharacters } from '../actions';
 
 class Episode extends Component {
-  constructor(props) {
-    super(props);
-  }
   componentDidMount() {
-    this.props.fetchEpisode(this.props.id);
+    if (this.props.episode.id !== Number(this.props.id)) this.props.fetchEpisode(this.props.id);
   }
   componentDidUpdate() {
     // fetch planets and character after load episode
     const { id, planets, planets_url, characters, characters_url } = this.props.episode;
-    if (id === Number(this.props.id)) {
-      if (planets.length === 0) {
-        planets_url.map((url) => {
-          this.props.fetchEpisodePlanets(url);
-          return url;
-        });
-      }
-      if (characters.length === 0) {
-        characters_url.map((url) => {
-          this.props.fetchEpisodeCharacters(url);
-          return url;
-        });
-      }
+    if (id === Number(this.props.id) && planets.length === 0 && characters.length === 0) {
+      planets_url.map((url) => {
+        this.props.fetchEpisodePlanets(url);
+        return url;
+      });
+      characters_url.map((url) => {
+        this.props.fetchEpisodeCharacters(url);
+        return url;
+      });
     }
   }
+
   render() {
     if (this.props.episode.id !== Number(this.props.id)) return <p>Loading...</p>;
-    
+    const {title, director, release_date, planets, planets_url, characters, characters_url } = this.props.episode;
+    const renderList = (name, url) => {
+      if (name.length !== url.length) return 'Loading...';
+      return (
+        <ul>
+          {name.map(item => (<li key={item}>{item}</li>))}
+        </ul>
+      );
+    };
     return (
       <div>
-        <h2>{this.props.episode.title}</h2>
+        <h2>{title}</h2>
         <ul>
-          <li>director: {this.props.episode.director}</li>
-          <li>release: {this.props.episode.release_date}</li>
+          <li>director: {director}</li>
+          <li>release: {release_date}</li>
+          <li>characters: {renderList(characters, characters_url)}</li>
+          <li>planets: {renderList(planets, planets_url)}</li>
         </ul>
       </div>
     );
